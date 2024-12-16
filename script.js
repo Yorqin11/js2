adb4b200623833a9105857b0212d0a6465750d87
Yorqin11 <yorqinturgunboyev5@gmail.com>
2024-12-16 16:48:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// console.log(typeof(String(5)));
+
+const savol1 = prompt('String typerdagi malumotni kiriting');
+const savol2 = prompt('Number typerdagi malumotni kiriting');
+const savol3 = prompt('Boolean typerdagi malumotni kiriting');
